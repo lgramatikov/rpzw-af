62d6c91af49b2f6b6ff34ed12fea3b8c1dc35d23
Lazar Gramatikov <lazar.gramatikov@gmail.com>
2017-07-08 08:35:50 +0300

    Remove required validation for message property

--- build-webhook/index.js
+++ build-webhook/index.js
@@ -1,6 +1,6 @@
 module.exports = function (context, req) {
     //at least some very basic validation
-    if (req.body && req.body.project && req.body.status && req.body.message) {
+    if (req.body && req.body.project && req.body.status) {
         context.bindings.outputSbMsg = req.body;
         context.bindings.outputSbMsg.contentType = "application/json";
     
@@ -11,7 +11,7 @@ module.exports = function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please specify all properties - project, status and message!"
+            body: "Please specify at least project and status properties. Message is optional."
         };
     }
 
